f05f10132373e0c2b8249a3a44f873cdb30242f5
pybmfc@gmail.com <pybmfc@gmail.com@18cc71b9-e20a-f1e7-4388-c9ae69c7ec2f>
2012-05-17 15:03:53 +0000

    删除资源文件

---
+++ trunk/系统模块/发布组件/服务器组件/Debug/kill.bat
@@ -0,0 +1,1 @@
+Taskkill /F /IM ServiceLoader.exe /IM LogonServer.exe /IM CenterServer.exe
---
+++ trunk/系统模块/发布组件/服务器组件/Debug/start.bat
@@ -0,0 +1,8 @@
+start CenterServer.exe
+ping 127.0.0.1 -n 5
+
+start LogonServer.exe
+ping 127.0.0.1 -n 5
+
+start ServiceLoader.exe
+ping 127.0.0.1 -n 5
