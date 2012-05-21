a967640737d661300b95f71b9ebfed3dee1ea867
pybmfc@gmail.com <pybmfc@gmail.com@18cc71b9-e20a-f1e7-4388-c9ae69c7ec2f>
2012-05-21 04:37:25 +0000

    6603脚本

---
+++ trunk/网狐6603/正版6603数据库脚本代码/脚本代码/存储过程/积分数据库/加载道具.sql
@@ -0,0 +1,41 @@
+
+----------------------------------------------------------------------------------------------------
+
+USE QPGameScoreDB
+GO
+
+IF EXISTS (SELECT * FROM DBO.SYSOBJECTS WHERE ID = OBJECT_ID(N'[dbo].[GSP_GR_LoadGameProperty]') and OBJECTPROPERTY(ID, N'IsProcedure') = 1)
+DROP PROCEDURE [dbo].[GSP_GR_LoadGameProperty]
+GO
+
+SET QUOTED_IDENTIFIER ON 
+GO
+
+SET ANSI_NULLS ON 
+GO
+
+----------------------------------------------------------------------------------------------------
+
+-- 加载道具
+CREATE PROC GSP_GR_LoadGameProperty
+	@wKindID SMALLINT,							-- 游戏 I D
+	@wServerID SMALLINT							-- 房间 I D
+WITH ENCRYPTION AS
+
+-- 属性设置
+SET NOCOUNT ON
+
+-- 执行逻辑
+BEGIN
+
+	-- 加载道具
+	SELECT ID, IssueArea, Cash, Gold, Discount, SendLoveLiness, RecvLoveLiness FROM GameProperty(NOLOCK)
+	WHERE Nullity=0 AND (IssueArea&6)<>0
+
+END
+
+RETURN 0
+
+GO
+
+----------------------------------------------------------------------------------------------------
